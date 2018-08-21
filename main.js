807cabfb48fc3a7a554f9ad5f300f7c28c0b274e
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2018-08-21 22:15:53 +0300

    changed the guy sprite

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function preload() {
     // second param - relative path to the location of the file
     // third param - width of each frame
     // fourth param - height of each frame
-    game.load.spritesheet('guy', 'assets/images/guy.png', 300 / 11, 34);
+    game.load.spritesheet('guy', 'assets/images/professor_walk_cycle_no_hat.png', 576/9, 256/4);
 
     // loads image
     game.load.image('star', 'assets/images/star.png');
@@ -66,8 +66,10 @@ function create() {
     // 2nd: array of frames which will be used for the animation
     // 3rd: framerate (frames per second)
     // 4th: if true loops through frames infinitely
-    guy.animations.add('walkLeft', [0, 1, 2, 3], 12, true);
-    guy.animations.add('walkRight', [4, 5, 6, 7, 8], 12, true);
+    guy.animations.add('walkUp', [0, 1, 2, 3, 4, 5, 6, 7, 8], 12, true);
+    guy.animations.add('walkLeft', [9, 10, 11, 12, 13, 14, 15, 16, 17], 12, true);
+    guy.animations.add('walkDown', [18, 19, 20, 21, 22, 23, 24, 25, 26], 12, true);
+    guy.animations.add('walkRight', [27, 28, 29, 30, 31, 32, 33, 34, 35], 12, true);
 
     // adds physics body to the HERO
     // so that we can collide with other objects
@@ -139,27 +141,28 @@ function update() {
     );
 
     // check for pressed keys
+    var speed = 5;
     if (isAnyCursorKeyPressed()) {
         if (cursors.right.isDown) {
-            guy.x += 10; // move to the right with 10 pixels
-            guy.rotation = 0; // look to the right;
+            guy.x += speed; // move to the right with 10 pixels
+            // guy.rotation = 0; // look to the right;
             // play the registered animation
             guy.animations.play('walkRight');
         }
         if (cursors.left.isDown) {
-            guy.x -= 10; // move to the left with 10 pixes
-            guy.rotation = 3.14; // look to the left
+            guy.x -= speed; // move to the left with 10 pixes
+            // guy.rotation = 3.14; // look to the left
             guy.animations.play('walkLeft');
         }
         if (cursors.up.isDown) {
-            guy.y -= 10;
-            guy.rotation = 3.14 * 3 / 2;
-            guy.animations.play('walkRight');
+            guy.y -= speed;
+            // guy.rotation = 3.14 * 3 / 2;
+            guy.animations.play('walkUp');
         }
         if (cursors.down.isDown) {
-            guy.y += 10;
-            guy.rotation = 3.14 / 2;
-            guy.animations.play('walkLeft');
+            guy.y += speed;
+            // guy.rotation = 3.14 / 2;
+            guy.animations.play('walkDown');
         }
     }
     else {
