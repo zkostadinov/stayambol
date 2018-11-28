60ae5745de2cb22bde7e61288efc60492533de5e
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2018-11-29 00:10:40 +0200

    added keyboard movement

--- lessons/snake/snake.js
+++ lessons/snake/snake.js
@@ -1,17 +1,27 @@
+var keySpace;
+var head;
+
 var game = new Phaser.Game(
-    '100', // width of canvas
-    '100', // height of canvas
+    '100%', // width of canvas
+    '100%', // height of canvas
     Phaser.CANVAS, // use CANVAS and not WEBGL
     'game', // name of the game
     // object with the three functions required for Phaser to work
     {
         preload: function() {
             game.load.image('head', 'spinning-snake-29.png');
+            game.load.image('background', 'grass.png');
         },
         create: function() {
-            game.stage.backgroundColor='#239933';
-            var head = game.add.sprite(10, 10, 'head', 1);
+            game.stage.background=game.add.sprite(0, 0, 'background', 1);
+            head = game.add.sprite(10, 10, 'head', 1);
             head.scale.setTo(0.1, 0.1);
+            keySpace = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+        },
+        update: () => {
+            if (keySpace.isDown) {
+                head.x += 10;
+            }            
         }
     }
 );
