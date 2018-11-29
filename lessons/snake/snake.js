7e003051a41e39cf66ab9360d8d6d3be91669d80
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2018-11-29 20:54:22 +0200

    last changes

--- lessons/snake/snake.js
+++ lessons/snake/snake.js
@@ -10,18 +10,40 @@ var game = new Phaser.Game(
     {
         preload: function() {
             game.load.image('head', 'spinning-snake-29.png');
-            game.load.image('background', 'grass.png');
         },
         create: function() {
-            game.stage.background=game.add.sprite(0, 0, 'background', 1);
+            game.stage.backgroundColor='#229944';
             head = game.add.sprite(10, 10, 'head', 1);
             head.scale.setTo(0.1, 0.1);
             keySpace = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+            keyUp = game.input.keyboard.addKey(Phaser.KeyCode.UP);
+            keyDown = game.input.keyboard.addKey(Phaser.KeyCode.DOWN);
+            keyLeft = game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
+            keyRight = game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
+
+            game.physics.enable(head, Phaser.Physics.ARCADE);
+            head.body.collideWorldBounds = true;
+            head.anchor.x = 0.5;
+            head.anchor.y = 0.5;        
         },
         update: () => {
-            if (keySpace.isDown) {
-                head.x += 10;
-            }            
+            var speed = 300;
+            if (keyUp.isDown) {
+                head.body.velocity.x = 0;
+                head.body.velocity.y = -speed;
+            }
+            if (keyDown.isDown) {
+                head.body.velocity.x = 0;
+                head.body.velocity.y = speed;
+            }
+            if (keyLeft.isDown) {
+                head.body.velocity.x = -speed;
+                head.body.velocity.y = 0;
+            }
+            if (keyRight.isDown) {
+                head.body.velocity.x = speed;
+                head.body.velocity.y = 0;
+            }
         }
     }
 );
