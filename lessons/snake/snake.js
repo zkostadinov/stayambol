f83aec72a8c4eb636b1aaff1686eacc6c28f8758
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2018-12-19 00:46:37 +0200

    added some apples

--- lessons/snake/snake.js
+++ lessons/snake/snake.js
@@ -1,9 +1,10 @@
-var keySpace;
 var head;
 var apples;
-var points = 0;
 var applesEatenBySnake;
 var text;
+
+var headTurningPoints = [];
+var MAX_HEAD_TURNING_POINTS = 10;
 
 var game = new Phaser.Game(
     '100%', // width of canvas
@@ -12,42 +13,48 @@ var game = new Phaser.Game(
     'game', // name of the game
     // object with the three functions required for Phaser to work
     {
-        preload: function() {
+        preload: function () {
             game.load.image('head', 'spinning-snake-29.png');
             game.load.image('apple', 'apple.png');
         },
         create: myCreateFunction,
         update: myUpdateFunction,
         render: () => {
-                // game.debug.spriteInfo(head, 10, 100);
-                // game.debug.bodyInfo(head, 10, 200);
+            // game.debug.spriteInfo(head, 10, 100);
+            // game.debug.bodyInfo(head, 10, 200);
 
-                // render the sprites bounds
-                /*if (head.getBounds()) {
-                    game.debug.rectangle(head.getBounds(), '#ffffff', false);
-                }
-                applesEatenBySnake.forEach(apple => {
-                    game.debug.rectangle(apple.getBounds(), '#ffffff', false);
-                });*/
-                
+            // game.debug.text(headTurningPoints, 10, 10);
+            game.debug.text('direction: ' + direction.x + ' / ' + direction.y, 500, 10);
+            game.debug.text('center: ' + head.position, 500, 30);
+            if (applesEatenBySnake.length > 0) {
+                game.debug.text('apple: ' + applesEatenBySnake[0].position, 500, 50);
+            }
+
+            // render the sprites bounds
+            /*if (head.getBounds()) {
+                game.debug.rectangle(head.getBounds(), '#ffffff', false);
+            }
+            applesEatenBySnake.forEach(apple => {
+                game.debug.rectangle(apple.getBounds(), '#ffffff', false);
+            });*/
+
         }
     }
 );
 
-function myCreateFunction() {        
-    game.stage.backgroundColor='#229944';
+function myCreateFunction() {
+    game.stage.backgroundColor = '#229944';
     head = game.add.sprite(10, 10, 'head', 1);
     head.scale.setTo(-0.1, 0.1);
-    keySpace = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-    keyUp = game.input.keyboard.addKey(Phaser.KeyCode.UP);
-    keyDown = game.input.keyboard.addKey(Phaser.KeyCode.DOWN);
-    keyLeft = game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
-    keyRight = game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
+    let keyUp = game.input.keyboard.addKey(Phaser.KeyCode.UP);
+    let keyDown = game.input.keyboard.addKey(Phaser.KeyCode.DOWN);
+    let keyLeft = game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
+    let keyRight = game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
 
     game.physics.enable(head, Phaser.Physics.ARCADE);
     head.body.collideWorldBounds = true;
     head.anchor.x = 0.5;
-    head.anchor.y = 0.5;        
+    head.anchor.y = 0.5;
 
     apples = [];
     apples.push(createApple(100, 100));
@@ -58,6 +65,47 @@ function myCreateFunction() {
     text = game.add.text(game.world.bounds.width - 200, 100, "");
 
     applesEatenBySnake = [];
+    eatenApplesOldLocations = [];
+
+    // movement handling code
+    var speed = 300;
+    function addHeadTurningPoint() {
+        headTurningPoints.unshift(new Phaser.Point(head.centerX, head.centerY));
+        if (headTurningPoints.length >= MAX_HEAD_TURNING_POINTS) {
+            headTurningPoints.splice(MAX_HEAD_TURNING_POINTS, headTurningPoints.length - MAX_HEAD_TURNING_POINTS);
+        }
+    }
+
+    keyUp.onDown.add(() => {
+        head.body.velocity.x = 0;
+        head.body.velocity.y = -speed;
+        head.rotation = Math.PI * 3 / 2;
+        head.scale.x = -0.1;
+        addHeadTurningPoint();
+    });
+    keyDown.onDown.add(() => {
+        head.body.velocity.x = 0;
+        head.body.velocity.y = speed;
+        head.scale.x = -0.1;
+        head.rotation = Math.PI / 2;
+        addHeadTurningPoint();
+    });
+    keyLeft.onDown.add(() => {
+        head.body.velocity.x = -speed;
+        head.body.velocity.y = 0;
+        head.scale.x = 0.1;
+        head.rotation = 0;
+        addHeadTurningPoint();
+    });
+    keyRight.onDown.add(() => {
+        head.body.velocity.x = speed;
+        head.body.velocity.y = 0;
+        head.scale.x = -0.1;
+        head.rotation = 0;
+        addHeadTurningPoint();
+    });
+    addHeadTurningPoint(); // remember the initial point as a turning point
+    // end of movement handling code
 }
 
 function createApple(x, y) {
@@ -68,53 +116,33 @@ function createApple(x, y) {
 }
 
 function myUpdateFunction() {
-    var speed = 300;
-    if (keyUp.isDown) {
-        head.body.velocity.x = 0;
-        head.body.velocity.y = -speed;
-        head.rotation = Math.PI * 3 / 2;
-        head.scale.x = -0.1;
-    }
-    if (keyDown.isDown) {
-        head.body.velocity.x = 0;
-        head.body.velocity.y = speed;
-        head.scale.x = -0.1;
-        head.rotation = Math.PI / 2;
-    }
-    if (keyLeft.isDown) {
-        head.body.velocity.x = -speed;
-        head.body.velocity.y = 0;
-        head.scale.x = 0.1;
-        head.rotation = 0;
-    }
-    if (keyRight.isDown) {
-        head.body.velocity.x = speed;
-        head.body.velocity.y = 0;
-        head.scale.x = -0.1;
-        head.rotation = 0;
-    }
-
     for (let i = 0; i < apples.length; i ++) {
         game.physics.arcade.collide(head, apples[i], snakeHitApple);
     }
 
-    text.setText("Точки: " + points);
+    current = new Phaser.Point(head.centerX, head.centerY);
+    var stack = headTurningPoints.slice().reverse();
 
-    var x = head.body.x + head.body.width;
-    var y = head.body.y;
+    // this considers only the last point
+    var previous = stack.pop();
+    direction = new Phaser.Point(
+        Math.sign(Math.round(previous.x - current.x)), 
+        Math.sign(Math.round(previous.y - current.y)));
+    var x = current.x + head.width * direction.x;
+    var y = current.y + head.height / 2 * direction.y;
     for (let i = 0; i < applesEatenBySnake.length; i ++) {
         let eaten = applesEatenBySnake[i];
-        eaten.x = x;
-        eaten.y = y;
-        x += eaten.width;
+        eaten.centerX = x;
+        eaten.centerY = y;
+        x += eaten.width * direction.x;
+        y += eaten.height * direction.y;
     }
 }
 
 function snakeHitApple(head, apple) {
     apples.splice(apples.indexOf(apple), 1);
-    points = points + 5;
     applesEatenBySnake.push(apple);
-    apple.body.velocity.setTo(0,0);
+    apple.body.velocity.setTo(0, 0);
 
     // scale the size of the apple to the size of the head
     apple.scale.setTo(1, 1);
