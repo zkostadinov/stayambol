6363cc9bc11317f6486bb0011b2e09dbfa43b2e6
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2019-01-10 22:59:45 +0200

    added bricks to breakout

--- lessons/breakout/breakout.js
+++ lessons/breakout/breakout.js
@@ -5,33 +5,50 @@ var game = new Phaser.Game(
     'game', // name of the game
     // object with the three functions required for Phaser to work
     {
-        preload: function() {
-            game.load.atlasXML('breakout', 
-                'Breakout_Tile_Free.png', 
+        preload: function () {
+            game.load.atlasXML('breakout',
+                'Breakout_Tile_Free.png',
                 'Breakout_Tile_Free.xml');
         },
         create: myCreateFunction,
         update: myUpdateFunction,
         render: () => {
-                //game.debug.text("hello world!", 10, 20);                
+            //game.debug.text("hello world!", 10, 20);                
         }
     }
 );
 
 var spaceKey;
 
-function myCreateFunction() {     
+function myCreateFunction() {
     pad = game.add.sprite(0, 0, 'breakout');
     pad.anchor.x = 0.5;
-    pad.animations.add('work', 
-        Phaser.Animation.generateFrameNames('', 50, 52, 
-                 '-Breakout-Tiles.png'), 
-            10, true);
+    pad.animations.add('work',
+        Phaser.Animation.generateFrameNames('', 50, 52,
+            '-Breakout-Tiles.png'),
+        10, true);
     pad.animations.play('work');
     pad.y = game.world.height - pad.height;
     game.physics.enable(pad, Phaser.Physics.ARCADE);
+    pad.body.immovable = true;
 
     spaceKey = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+    // добавяме тухлички
+    var countBricks = 12;
+    const brickWidthDistanceProportion = 1.3;
+    var brickWidth = game.world.width / (countBricks * brickWidthDistanceProportion + 0.9);
+    var x = brickWidth / 2;
+    for (let i = 0; i < countBricks; i ++) {
+        createBrick(x, brickWidth);
+        x = x + brickWidth * brickWidthDistanceProportion;
+    }
+}
+
+function createBrick(x, width) {
+    var brick = game.add.sprite(x, 0, 'breakout', '01-Breakout-Tiles.png');
+    brick.height = brick.height * width / brick.width;
+    brick.width = width;
 }
 
 var ball;
@@ -55,12 +72,28 @@ function myUpdateFunction() {
             ball.x = pad.x;
             ball.y = pad.y - ball.height;
             game.physics.enable(ball, Phaser.Physics.ARCADE);
-            ball.body.velocity.y = -200;
+            ball.body.velocity.y = -500;
             ball.body.velocity.x = -80;
             ball.body.collideWorldBounds = true;
             ball.body.bounce.set(1);
+
+            // ball dies when reaches bottom
+            // game.physics.arcade.checkCollision.down = false;
+            // ball.checkWorldBounds = true;
+            // ball.events.onOutOfBounds.add(()=>{console.log('TODO: finish')}, this);
         }
     }
 
-    
+    game.physics.arcade.collide(ball, pad, reflectBallFromPad, null, this);
+}
+
+function reflectBallFromPad(ball, pad) {
+    var x1, y1, x2, y2, A;
+    x1 = ball.body.velocity.x;
+    y1 = ball.body.velocity.y;
+    A = 2 * (ball.x - pad.x);
+    x2 = x1 + A;
+    y2 = - Math.sqrt(x1*x1 + y1*y1 - x2*x2);
+    ball.body.velocity.x = x2;
+    ball.body.velocity.y = y2;
 }
