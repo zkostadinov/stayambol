eebddd825696a5b169780228566d0fd6f74f0cde
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2019-02-10 17:28:42 +0200

    mushroom jumping

--- tiles-example/tiles.js
+++ tiles-example/tiles.js
@@ -8,10 +8,10 @@ var game = new Phaser.Game(
     {
         // зарежда начални данни в паметта
         preload: () => {
-            // game.load.tilemap('map', 'map.json', null, Phaser.Tilemap.TILED_JSON);
-            // game.load.image('tiles', 'simples_pimples.png');
-            game.load.tilemap('map', 'super_mario.json', null, Phaser.Tilemap.TILED_JSON);
-            game.load.image('tiles', 'super_mario.png');
+            // зареждаме картата
+            game.load.tilemap('map', 'map.json', null, Phaser.Tilemap.TILED_JSON);
+            game.load.image('tiles', 'simples_pimples.png');
+            game.load.image('player', '../test/assets/images/mushroom.png');
         },
         // вика се при стартиране на играта
         create: create,
@@ -24,14 +24,50 @@ var game = new Phaser.Game(
 );
 
 function create() {
-    game.stage.backgroundColor = '#787878';
+    // добавяме картата към играта
     map = game.add.tilemap('map');
-    // map.addTilesetImage('simples_pimples.tsx', 'tiles');
-    // layer = map.createLayer('Tile Layer 1');
-    map.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
-    layer = map.createLayer('World1');
-    layer.resizeWorld();
+    // сменяме размера на екрана според картата
+    game.scale.setGameSize(map.widthInPixels, map.heightInPixels);
+    // казваме къде са картинките
+    map.addTilesetImage('simples_pimples', 'tiles');
+    // добавяме единия слой с картинки
+    layer = map.createLayer('Tile Layer 1');
+
+    // казваме на обектите да се блъскат в нашите спрайтове
+    // map.setCollision(108);
+    map.setCollision(201);
+    map.setCollision(251);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    player = game.add.sprite(32, 32, 'player');
+    player.scale.setTo(0.1, 0.1);
+    game.physics.enable(player);
+
+    // включваме някаква гравитация за играта
+    game.physics.arcade.gravity.y = 250;
 }
 
 function update() {
+    // играчът да се сблъсква с нашия слой
+    game.physics.arcade.collide(player, layer);
+
+    // малко да го раздвижим
+
+    if (cursors.up.isDown)
+    {
+        if (player.body.onFloor())
+        {
+            player.body.velocity.y = -200;
+        }
+    }
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 150;
+    }
 }
