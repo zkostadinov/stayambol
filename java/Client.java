f45dee55655f461f99e89cc19917ba4b394c23be
Zlatko Kostadinov <kostadinov.zlatko@gmail.com>
2019-04-25 19:31:07 +0300

    updated client for jdk7

--- java/Client.java
+++ java/Client.java
@@ -3,11 +3,19 @@ import java.net.Socket;
 class Client {
     public static void main(String[] args) throws Exception {
         System.out.println("Connecting to server...");
-        Socket socket = new Socket("localhost", 8000);
+        Socket connection = new Socket("localhost", 8000);
         System.out.println("Connected.");
         System.out.println("Requesting index page content.");
-        socket.getOutputStream().write("GET /\n\n".getBytes());
-        byte[] response = socket.getInputStream().readAllBytes();
-        System.out.println(new String(response));
+        connection.getOutputStream().write("GET /\n\n".getBytes());
+//        byte[] response = connection.getInputStream().readAllBytes();
+        byte[] response = new byte[10];
+        while(true) {
+            int countRead = connection.getInputStream().read(response);
+            System.out.println(countRead);
+            if (countRead == -1) {
+                break;
+            }
+            System.out.println(new String(response, 0, countRead));
+        }
     }
 }
